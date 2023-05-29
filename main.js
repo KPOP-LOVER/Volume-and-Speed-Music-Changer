f995d6374335749ba523143a4109e77b0d4c42a4
KPOP-LOVER <100190557+KPOP-LOVER@users.noreply.github.com>
2023-05-29 19:09:19 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-var song="";
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
 function setup(){
-    canvas=createCanvas(600,500);
+    canvas = createCanvas(600,530);
     canvas.center();
+
     video = createCapture(VIDEO);
     video.hide();
 }
-function draw(){
-    image(video,0,0,600,500);
-}
+
 function preload(){
-    song=loadSound("music.mp3");
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
 }
-function play(){
-    song.play();
+
+function draw(){
+    image(video,0,0,600,530);
 }
